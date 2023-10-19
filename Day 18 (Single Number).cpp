036766fd29be44125fdfca2a6f5631030c055208
soumya2345698 <116783515+soumya2345698@users.noreply.github.com>
2023-10-20 00:08:44 +0530

    Add files via upload

---
+++ Day 18 (Single Number).cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int n;
+
+        int ans=0;
+
+        n=nums.size();
+        for(int i=0;i<n;i++){
+
+          ans^=nums[i];
+        }
+        return ans;
+    }
+};
