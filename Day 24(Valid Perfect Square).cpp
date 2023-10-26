25b9d6c5cd481d1513f6298b6cfd4578dfe2922d
soumya2345698 <116783515+soumya2345698@users.noreply.github.com>
2023-10-26 10:05:08 +0530

    Add files via upload

---
+++ Day 24(Valid Perfect Square).cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    double ronakSqrt(int n)
+    {
+        if(n<2)
+            return double(n);
+        
+        
+        double sqrt = n / 2;
+        double temp = 0;
+
+        // Iterate until sqrt is different of temp, that is updated on the loop
+        while(sqrt != temp){
+  
+            temp = sqrt;
+
+
+            sqrt = ( n/temp + temp) / 2;
+        }
+
+        return sqrt;
+    }
+
+    bool isPerfectSquare(int num) 
+    {
+        double a=ronakSqrt(num);
+        double b=ceil(ronakSqrt(num));
+        cout<<a<<endl<<b<<endl;
+        if(a == b)
+            return true;
+        return false;
+    }
+};
