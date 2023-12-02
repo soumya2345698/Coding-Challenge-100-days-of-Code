b9c1a3869afbf1298426558c9d8ea0a0b33dd255
soumya2345698 <116783515+soumya2345698@users.noreply.github.com>
2023-12-02 10:37:50 +0530

    Add files via upload

---
+++ Day 61 (Check if Two Array Strings are Equivalent).cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
+
+        string s1="";
+        string s2="";
+
+        for(auto x: word1)
+        {
+            s1+=x;
+        }
+        for(auto y: word2)
+        {
+            s2+=y;
+        }
+        if(s1==s2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+};
